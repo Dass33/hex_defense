33645fc6331275e268b0fe3d143db48d189ca030
Daniel Dvořák <dvorad35@fit.cvut.cz>
2025-02-05 22:21:03 +0100

    make level saving and loading work

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,8 @@ public:
     [[nodiscard]] char at(int x, int y) const;
 
     void edit_level();
-    void print_level() const;
+    void print_level(WINDOW *win) const;
+    void save_level(std::string& level_path) const;
     friend void get_level(Level &level, std::string level_path);
 private:
         std::vector<char> tiles;
@@ -36,54 +37,69 @@ Level::Level() {
     getmaxyx(stdscr, yMax, xMax);
     winHeight = yMax / (3.0/2);
     winWidth = xMax / (3.0/2);
-    tiles.resize((winWidth-1) * (winHeight-1), ' ');
+    // should allocate less memory
+    tiles.resize((winWidth-2) * (winHeight-2), ' ');
 }
 
 void Level::insert(const int x, const int y, const int val) {
-    tiles[y * (winWidth-2) + x] = val;
+    tiles[(y-1) * (winWidth-2) + (x-1)] = val;
 }
  
 char Level::at(const int x, const int y) const {
-    return tiles[y * (winWidth-2) + x];
+    return tiles[(y-1) * (winWidth-2) + (x-1)];
 }
 
-void Level::print_level() const{
+void Level::save_level(std::string& level_path) const{
+    std::ofstream ofs(level_path);
+    ofs << winHeight << ' ' << winWidth << ' ';
+    for(size_t i = 0; i < tiles.size(); i++) {
+        ofs.put(tiles[i]);
+    }
+    ofs.close();
+}
+
+void Level::print_level(WINDOW *win) const{
     //todo improve printing
-    WINDOW *level_win = newwin(winHeight, winWidth, yMax/6,xMax/6);
-    box(level_win,0,0);
+    box(win,0,0);
 
     int e = 1;
     for (size_t i = 0; i < tiles.size(); i++) {
         if (i % (winWidth-2) == 0) {
-            wmove(level_win,e++,1);
+            wmove(win,e++,1);
         }
-        wprintw(level_win, "%c", tiles[i]);
+        wprintw(win, "%c", tiles[i]);
     }
-    wgetch(level_win);
 }
 
 bool is_valid_level(Level& level) {
-    if (!isprint(level.at(0,0))) return false;
+    //todo
+    //if (!isprint(level.at(0,0))) return false;
     return true;
 }
 
+
 void get_level(Level &level, std::string level_path) {
     try {
-        //if (!std::filesystem::exists(level_path)) return;
-        size_t file_size = std::filesystem::file_size(level_path);
-        if (file_size != sizeof(Level)) return;
-
-        std::ifstream ifs(level_path.data(), std::ios::binary);
+        std::ifstream ifs(level_path.data());
         if (!ifs.good()) return;
 
-        Level tmp_level;
+        size_t winHeight, winWidth;
+        if (!(ifs >> winHeight >> winWidth)) return;
+        if (winHeight < MIN_LEVEL_HEIGHT || winWidth < MIN_LEVEL_WIDTH) return;
+        if (winHeight > MAX_LEVEL_HEIGHT || winWidth > MAX_LEVEL_WIDTH) return;
 
-        ifs.read(reinterpret_cast<char*>(&tmp_level), file_size);
+        ifs.get();
+        std::vector<char> tmp_vec;
+        for (size_t i = 0; i < (winWidth -2) * (winHeight -2);i++) {
+            char tmp_char;
+            if (!ifs.get(tmp_char)) return;
+            tmp_vec.push_back(tmp_char);
+        }
         ifs.close();
-
-        if (!is_valid_level(tmp_level)) return;
-
-        level = tmp_level;
+        
+        level.winHeight = winHeight;
+        level.winWidth = winWidth;
+        level.tiles = std::move(tmp_vec);
 
         getmaxyx(stdscr, level.yMax, level.xMax);
         level.winHeight = level.yMax / (3.0/2);
@@ -95,7 +111,9 @@ void get_level(Level &level, std::string level_path) {
 
 void Level::edit_level() {
     WINDOW *level_editor = newwin(winHeight, winWidth, yMax/6,xMax/6);
-    box(level_editor, 0, 0);
+
+    print_level(level_editor);
+    //box(level_editor, 0, 0);
 
     keypad(level_editor, true);
 
@@ -106,7 +124,8 @@ void Level::edit_level() {
     bool print_toggle = false;
 
     mvwprintw(level_editor,1,1,"@");
-    mvprintw(yMax-1,1,"Last input: ");
+    //mvprintw(yMax-1,1,"Last input: ");
+    wrefresh(level_editor);
     do {
         c = wgetch(level_editor);
         // 10 is the code for Enter, KEY_ENTER did not work
@@ -152,12 +171,15 @@ void Level::edit_level() {
         mvprintw(yMax-1,1,"Last input: %c ", c);
         mvprintw(yMax-2,1,"at(x,y) = '%c'", at(x,y));
         mvprintw(yMax-3,1,"print char: %c ", print_char);
+        mvprintw(yMax-4,1,"pos %ld ", (y-1) * (winWidth-2) + (x-1));
         refresh();
     } while (c != 'q');
     //wclear(level_editor);
 
 }
 
+//todo open level with right size
+//or forbid opening it
 void level_menu(WINDOW* menu) {
     int yMax, xMax;
     getmaxyx(stdscr, yMax, xMax);
@@ -193,8 +215,8 @@ void level_menu(WINDOW* menu) {
 
     Level level;
     get_level(level, level_path);
-    //level.print_level();
     level.edit_level();
+    level.save_level(level_path);
     wclear(menu);
 }
 
